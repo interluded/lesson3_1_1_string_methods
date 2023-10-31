0d90949068757dc8a6fefb9afc3736c4b31920e5
marclmao <119444616+marclmao@users.noreply.github.com>
2023-10-31 09:43:06 -0400

    asdasd

---
+++ src/FistLast.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FistLast {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("put in word");
+        String word = scan.nextLine();
+        int length = word.length();
+        char last_letter = word.charAt(length - 1);
+        char first_letter = word.charAt(0);
+
+        if (first_letter == last_letter){
+            System.out.println("same");
+        }
+        else{
+            System.out.println("diff");
+        }
+        }
+
+    }
