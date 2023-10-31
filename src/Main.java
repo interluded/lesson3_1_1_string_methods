f34ae3d1c33dfee0a5749eb530d0697bd46c7ab2
marclmao <119444616+marclmao@users.noreply.github.com>
2023-10-31 09:35:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String password1 = scan.nextLine();
+        int length = password1.length();
+        if (length > 6){
+            System.out.println("Invalid");
+        }
+        else {
+            System.out.println("good");
+        }
+
+    }
+}
