7e90842e0c612acf38951c74548d383d8915417f
marclmao <119444616+marclmao@users.noreply.github.com>
2023-10-31 09:54:16 -0400

    b01

---
+++ src/Password2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Password2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String password1 = scan.nextLine();
+        int length = password1.length();
+        char first_letter = password1.charAt(0);
+        boolean lowerCase = Character.isLowerCase(first_letter);
+        if (length > 6 || lowerCase == false){
+            System.out.println("Valid");
+        }
+        else {
+            System.out.println("false");
+        }
+
+    }
+}
